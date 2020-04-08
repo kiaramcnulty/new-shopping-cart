8fdef9c4f8e4c2acbd131195a809c5c1feb7e01d
Kiara McNulty <kiaramcnulty@gmail.com>
2020-04-08 08:59:58 -0700

    set up firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7iUmHBQvbOZr0ga8haB95shBjf8XMWEY",
+  authDomain: "new-shopping-cart-a16c2.firebaseapp.com",
+  databaseURL: "https://new-shopping-cart-a16c2.firebaseio.com",
+  projectId: "new-shopping-cart-a16c2",
+  storageBucket: "new-shopping-cart-a16c2.appspot.com",
+  messagingSenderId: "36715289431",
+  appId: "1:36715289431:web:8cbf007e47dc923ed63c12",
+  measurementId: "G-EM6H8L7Q6X"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
